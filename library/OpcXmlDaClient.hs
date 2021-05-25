8489d2c998ff15fad228b225d24dab09c9851115
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-05-25 23:19:22 +0300

    Stub out parts of the public module

--- library/OpcXmlDaClient.hs
+++ library/OpcXmlDaClient.hs
@@ -1,6 +1,26 @@
 module OpcXmlDaClient
 (
+  -- * Operations
+  Op,
+  getStatus,
+  -- * Data types
+  module OpcXmlDaClient.Protocol,
 )
 where
 
 import OpcXmlDaClient.Prelude
+import OpcXmlDaClient.Protocol
+
+
+
+data Op a
+
+instance Functor Op
+
+instance Applicative Op
+
+instance Monad Op
+
+
+getStatus :: GetStatus -> Op GetStatusResponse
+getStatus = error "TODO"
