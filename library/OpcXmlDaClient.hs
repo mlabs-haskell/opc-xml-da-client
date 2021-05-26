2ec08903f4b71726145da8328d51644c592e98fa
Koz Ross <koz.ross@retro-freedom.nz>
2021-05-26 12:28:51 +1200

    Cleanup, fill out entire API

--- library/OpcXmlDaClient.hs
+++ library/OpcXmlDaClient.hs
@@ -1,26 +1,65 @@
+{-# LANGUAGE DerivingVia #-}
+
 module OpcXmlDaClient
-(
-  -- * Operations
-  Op,
-  getStatus,
-  -- * Data types
-  module OpcXmlDaClient.Protocol,
-)
+  ( -- * Operations
+    Op,
+    getStatus,
+    read,
+    write,
+    subscribe,
+    subscriptionPolledRefresh,
+    subscriptionCancel,
+    browse,
+    getProperties,
+
+    -- * Data types
+    module OpcXmlDaClient.Protocol,
+  )
 where
 
-import OpcXmlDaClient.Prelude
+import OpcXmlDaClient.Prelude hiding (Read, read)
 import OpcXmlDaClient.Protocol
 
+-- @since 0.1
+newtype Op a = Op (Identity a)
+  deriving
+    ( -- | @since 0.1
+      Functor,
+      -- | @since 0.1
+      Applicative,
+      -- | @since 0.1
+      Monad
+    )
+    via Identity
 
-
-data Op a
-
-instance Functor Op
-
-instance Applicative Op
-
-instance Monad Op
-
-
+-- | @since 0.1
 getStatus :: GetStatus -> Op GetStatusResponse
 getStatus = error "TODO"
+
+-- | @since 0.1
+read :: Read -> Op ReadResponse
+read = error "TODO"
+
+-- | @since 0.1
+write :: Write -> Op WriteResponse
+write = error "TODO"
+
+-- | @since 0.1
+subscribe :: Subscribe -> Op SubscribeResponse
+subscribe = error "TODO"
+
+-- | @since 0.1
+subscriptionPolledRefresh :: SubscriptionPolledRefresh -> Op SubscriptionPolledRefreshResponse
+subscriptionPolledRefresh = error "TODO"
+
+-- | @since 0.1
+subscriptionCancel :: SubscriptionCancel -> Op SubscriptionCancelResponse
+subscriptionCancel = error "TODO"
+
+-- | @since 0.1
+browse :: Browse -> Op BrowseResponse
+browse = error "TODO"
+
+-- | @since 0.1
+getProperties :: GetProperties -> Op GetPropertiesResponse
+getProperties = error "TODO"
--- library/OpcXmlDaClient/Protocol.hs
+++ library/OpcXmlDaClient/Protocol.hs
@@ -1,24 +1,54 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module OpcXmlDaClient.Protocol where
 
-import OpcXmlDaClient.Prelude
+import Data.Scientific (Scientific)
+import Data.Time (LocalTime, TimeZone)
+import Data.Vector (Vector)
+import Data.Vector.Instances ()
 import Domain
-
+  ( accessorIsLabelDeriver,
+    boundedDeriver,
+    constructorIsLabelDeriver,
+    dataDeriver,
+    declare,
+    enumDeriver,
+    eqDeriver,
+    genericDeriver,
+    hasFieldDeriver,
+    hashableDeriver,
+    loadSchema,
+    mapperIsLabelDeriver,
+    ordDeriver,
+    showDeriver,
+    typeableDeriver,
+  )
+import OpcXmlDaClient.Prelude hiding (Read)
 
 declare
   (Just (True, False))
-  (mconcat [
-    enumDeriver,
-    boundedDeriver,
-    showDeriver,
-    eqDeriver,
-    ordDeriver,
-    genericDeriver,
-    dataDeriver,
-    typeableDeriver,
-    hashableDeriver,
-    hasFieldDeriver,
-    constructorIsLabelDeriver,
-    accessorIsLabelDeriver,
-    mapperIsLabelDeriver
-    ])
+  ( mconcat
+      [ enumDeriver,
+        boundedDeriver,
+        showDeriver,
+        eqDeriver,
+        ordDeriver,
+        genericDeriver,
+        dataDeriver,
+        typeableDeriver,
+        hashableDeriver,
+        hasFieldDeriver,
+        constructorIsLabelDeriver,
+        accessorIsLabelDeriver,
+        mapperIsLabelDeriver
+      ]
+  )
   =<< loadSchema "schemas/protocol.yaml"
