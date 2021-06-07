9bf63f699ae22447852255ac7e2bdc5f8032fadf
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-06-07 16:15:38 +0300

    Add parser stubs

---
+++ library/OpcXmlDaClient/Protocol/XmlParsing.hs
@@ -0,0 +1,33 @@
+module OpcXmlDaClient.Protocol.XmlParsing where
+
+import OpcXmlDaClient.Prelude hiding (Read)
+import OpcXmlDaClient.Protocol.Types
+import qualified Text.XML as Xml
+
+-- |
+-- Parsing error details and hopefully a location.
+data Error
+
+getStatusResponse :: Xml.Element -> Either Error GetStatusResponse
+getStatusResponse = error "TODO"
+
+readResponse :: Xml.Element -> Either Error ReadResponse
+readResponse = error "TODO"
+
+writeResponse :: Xml.Element -> Either Error WriteResponse
+writeResponse = error "TODO"
+
+subscribeResponse :: Xml.Element -> Either Error SubscribeResponse
+subscribeResponse = error "TODO"
+
+subscriptionPolledRefreshResponse :: Xml.Element -> Either Error SubscriptionPolledRefreshResponse
+subscriptionPolledRefreshResponse = error "TODO"
+
+subscriptionCancelResponse :: Xml.Element -> Either Error SubscriptionCancelResponse
+subscriptionCancelResponse = error "TODO"
+
+browseResponse :: Xml.Element -> Either Error BrowseResponse
+browseResponse = error "TODO"
+
+getPropertiesResponse :: Xml.Element -> Either Error GetPropertiesResponse
+getPropertiesResponse = error "TODO"
