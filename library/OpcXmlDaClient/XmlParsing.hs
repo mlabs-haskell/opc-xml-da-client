8e323c8520d52240a871e43ba6ff92c089adddcc
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-06-07 16:38:53 +0300

    Correct the stubs to be on documents

--- library/OpcXmlDaClient/XmlParsing.hs
+++ library/OpcXmlDaClient/XmlParsing.hs
@@ -8,26 +8,26 @@ import qualified Text.XML as Xml
 -- Parsing error details and hopefully a location.
 data Error
 
-getStatusResponse :: Xml.Element -> Either Error GetStatusResponse
+getStatusResponse :: Xml.Document -> Either Error GetStatusResponse
 getStatusResponse = error "TODO"
 
-readResponse :: Xml.Element -> Either Error ReadResponse
+readResponse :: Xml.Document -> Either Error ReadResponse
 readResponse = error "TODO"
 
-writeResponse :: Xml.Element -> Either Error WriteResponse
+writeResponse :: Xml.Document -> Either Error WriteResponse
 writeResponse = error "TODO"
 
-subscribeResponse :: Xml.Element -> Either Error SubscribeResponse
+subscribeResponse :: Xml.Document -> Either Error SubscribeResponse
 subscribeResponse = error "TODO"
 
-subscriptionPolledRefreshResponse :: Xml.Element -> Either Error SubscriptionPolledRefreshResponse
+subscriptionPolledRefreshResponse :: Xml.Document -> Either Error SubscriptionPolledRefreshResponse
 subscriptionPolledRefreshResponse = error "TODO"
 
-subscriptionCancelResponse :: Xml.Element -> Either Error SubscriptionCancelResponse
+subscriptionCancelResponse :: Xml.Document -> Either Error SubscriptionCancelResponse
 subscriptionCancelResponse = error "TODO"
 
-browseResponse :: Xml.Element -> Either Error BrowseResponse
+browseResponse :: Xml.Document -> Either Error BrowseResponse
 browseResponse = error "TODO"
 
-getPropertiesResponse :: Xml.Element -> Either Error GetPropertiesResponse
+getPropertiesResponse :: Xml.Document -> Either Error GetPropertiesResponse
 getPropertiesResponse = error "TODO"
