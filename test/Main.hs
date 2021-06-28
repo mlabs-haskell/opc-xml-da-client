575ab5290416500469f152904941b27c5490f208
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-06-28 10:35:59 +0300

    Implement test

--- test/Main.hs
+++ test/Main.hs
@@ -3,19 +3,21 @@ module Main where
 import qualified Data.ByteString.Lazy as LazyByteString
 import qualified OpcXmlDaClient.XmlParsing as XmlParsing
 import qualified Test.QuickCheck as QuickCheck
+import OpcXmlDaClient.Types
 import Test.QuickCheck.Instances
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 import Test.Tasty.Runners
 import qualified XmlUnscrambler as Xu
-import Prelude hiding (assert)
+import Prelude
 
 main =
   defaultMain $
-    testGroup "All tests" $
-      [ testCase "" $ do
-          res <- Xu.parseFile XmlParsing.subscribeResponse "samples/680.response.xml"
-          let expected = Left ""
-           in assertEqual "" expected res
+    testGroup
+      "Parsers"
+      [ testCase "SubscribeResponse" $ do
+          Right x <- Xu.parseFile XmlParsing.subscribeResponse "samples/680.response.xml"
+          assertEqual "" (Just "Handle1") (#serverSubHandle x)
+          assertEqual "" (Just (read "2019-09-23 16:01:50.576+00:00")) (fmap #rcvTime (#subscribeResult x))
       ]
