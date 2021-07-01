3c68eef0fa1ad6d61a695c78760f51bc75053421
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-07-01 15:03:33 +0300

    Add manyWithIndex and manyWithIndexTerminating

--- base/OpcXmlDaClient/Base/Vector.hs
+++ base/OpcXmlDaClient/Base/Vector.hs
@@ -26,12 +26,30 @@ initialized size initialize = runST $ do
 -- Efficiently build a vector with the @many@ pattern.
 many :: (MonadPlus m, Vector v a) => m a -> m (v a)
 many produce =
+  manyWithIndex (const produce)
+
+manyWithIndex :: (MonadPlus m, Vector v a) => (Int -> m a) -> m (v a)
+manyWithIndex produce =
   build [] 0
   where
     build list !size =
       step <|> finish
       where
         step =
-          produce >>= \a -> build (a : list) (succ size)
+          produce size >>= \a -> build (a : list) (succ size)
         finish =
           pure (fromReverseListN size list)
+
+manyWithIndexTerminating :: (MonadPlus m, Vector v a) => (Int -> m (Either e a)) -> m (Either e (v a))
+manyWithIndexTerminating produce =
+  build [] 0
+  where
+    build list !size =
+      step <|> finish
+      where
+        step =
+          produce size >>= \case
+            Right a -> build (a : list) (succ size)
+            Left a -> return (Left a)
+        finish =
+          pure (Right (fromReverseListN size list))
