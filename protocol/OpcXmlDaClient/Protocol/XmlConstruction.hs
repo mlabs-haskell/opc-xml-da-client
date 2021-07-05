c58175d9a272b6bcd30f3bce1c0294c936e36348
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-07-05 10:09:20 +0300

    Add rendering of non-standard types

--- protocol/OpcXmlDaClient/Protocol/XmlConstruction.hs
+++ protocol/OpcXmlDaClient/Protocol/XmlConstruction.hs
@@ -215,7 +215,7 @@ valueElement elementName x =
             X.elementNode $ valueElement "anyType" x
           Nothing ->
             X.elementNode $ X.element (opcQName "anyType") [(xsiQName "isNil", "true")] []
-    NonStandardValue _ -> error "TODO"
+    NonStandardValue (ValueNonStandard a b) -> element (qNameQName a) (fmap X.astNode b)
   where
     element typeQName =
       X.element (opcQName elementName) [(xsiQName "type", X.qNameContent typeQName)]
