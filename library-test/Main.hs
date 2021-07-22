818f77ff3482e322f7ecc9fea6bb0c4aedb7d604
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-07-22 19:57:16 +0300

    Reproduce the get status request error

--- library-test/Main.hs
+++ library-test/Main.hs
@@ -1,10 +1,22 @@
 module Main where
 
+import Data.Default
+import qualified Network.HTTP.Client as Hc
+import qualified OpcXmlDaClient as Opc
 import Test.QuickCheck.Instances ()
 import Test.Tasty
+import Test.Tasty.HUnit
 import Prelude
 
 main =
   defaultMain $
-    testGroup "" $
-      []
+    testGroup "Mocking server interactions" $
+      let uri =
+            fromJust . Opc.textUri $
+              "http://info.advosol.com/XMLDADemo/XML_Sim/OpcXmlDaServer.asmx"
+          manager = unsafePerformIO $ Hc.newManager Hc.defaultManagerSettings
+          op _op = _op manager def uri
+       in [ testCase "GetStatus" $ do
+              _res <- op Opc.getStatus $ Opc.GetStatus Nothing Nothing
+              assertBool (show _res) $ isRight _res
+          ]
